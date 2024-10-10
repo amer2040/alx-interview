e8fd0c347fd7dc1088d95119fec399f38fcf89a2
amer2040 <aboodamer7@gmail.com>
2024-10-10 14:41:25 +0300

    update

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """def doc"""
+    n = len(boxes)
+    opened = [False] * n  # list to track boxes
+    opened[0] = True  # first box is unlocked always
+    keys = [0]  # the keys we have started from 0
+
+    while keys:
+        key = keys.pop(0)  #  take key from the keys list
+        for new_key in boxes[key]:  # we get the keys from the opend box
+            if new_key < n and not opened[new_key]:  # if the new box not opend
+                opened[new_key] = True  # we open it
+                keys.append(new_key)  # we add the key to the keys list
+
+    return all(opened)  # return True if all boxes are opened
