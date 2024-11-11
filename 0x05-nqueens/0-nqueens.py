d44d4d4cd601d9ae23cf3d9dff9a34a79d4ac050
amer2040 <aboodamer7@gmail.com>
2024-11-11 10:44:57 +0200

    update

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col):
+    for i in range(row):
+        if board[i] == col or abs(board[i] - col) == row - i:
+            return False
+    return True
+
+def solve_nqueens(N, board, row, solutions):
+    if row == N:
+        solutions.append(board[:])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(N, board, row + 1, solutions)
+
+def print_solutions(N):
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solutions = []
+    solve_nqueens(N, board, 0, solutions)
+
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(N)])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    N = sys.argv[1]
+    print_solutions(N)
